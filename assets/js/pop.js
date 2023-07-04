20ba342db359837749dcc310b0f49732860ad1f9
Samarth Chauhan <samarthchauhan703@icloud.com>
2023-07-05 00:02:06 +0530

    update

---
+++ assets/js/pop.js
@@ -0,0 +1,15 @@
+const popupScreen = document.querySelector(".popup-screen");
+const popupBox = document.querySelector(".popup-box");
+const closeButton = document.querySelector(".close-button");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    popupScreen.classList.add("active");
+    document.getElementById("body").style.overflow = "hidden";
+}, 0);
+});
+closeButton.addEventListener("click", () => {
+  popupScreen.classList.remove("active");
+  document.getElementById("body").style.overflow = "scroll";
+});
+
