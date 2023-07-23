a4ef5d5ebcc4ea11d09fa29b542ba99baf1d6fa9
Samarth Chauhan <samarthchauhan703@icloud.com>
2023-07-23 11:59:26 +0530

    nfnkdfznd

--- assets/js/pop.js
+++ assets/js/pop.js
@@ -6,10 +6,12 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     popupScreen.classList.add("active");
     document.getElementById("body").style.overflow = "hidden";
+    
 }, 1000);
 });
 closeButton.addEventListener("click", () => {
   popupScreen.classList.remove("active");
   document.getElementById("body").style.overflow = "auto";
+  
 });
 
