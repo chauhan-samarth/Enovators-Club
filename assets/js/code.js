d5ba8fc677a8190671562d1c71b28b322814394d
Samarth Chauhan <samarthchauhan703@icloud.com>
2023-07-29 12:37:33 +0530

    vjszbdjvbjvbjz

--- assets/js/code.js
+++ assets/js/code.js
@@ -3,35 +3,13 @@
 var _translations = _translations || {};
 Object.defineProperty(Number.prototype, 'fileSize', {
     value: function (a, b, c, d) {
-        return (a = a ? [1e3, 'k', 'B'] : [1024, 'K', 'b'], b = Math, c = b.log,
-                d = c(this) / c(a[0]) | 0, this / b.pow(a[0], d)).toFixed(0)
-            + ' ' + (d ? (a[1] + 'MGTPEZY')[--d] + a[2] : 'Bytes');
+        
     }, writable: false, enumerable: false
 });
 
 var Base64 = {
     _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
     encode: function (e) {
-        var t = "";
-        var n, r, i, s, o, u, a;
-        var f = 0;
-        e = Base64._utf8_encode(e);
-        while (f < e.length) {
-            n = e.charCodeAt(f++);
-            r = e.charCodeAt(f++);
-            i = e.charCodeAt(f++);
-            s = n >> 2;
-            o = (n & 3) << 4 | r >> 4;
-            u = (r & 15) << 2 | i >> 6;
-            a = i & 63;
-            if (isNaN(r)) {
-                u = a = 64
-            } else if (isNaN(i)) {
-                a = 64
-            }
-            t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
-        }
-        return t
     },
     decode: function (e) {
         var t = "";
