37347eefe7734746afe098114e45f19d3b840ede
Samarth Chauhan <samarthchauhan703@icloud.com>
2023-08-25 17:15:42 +0530

    ddfdfdfdfd

---
+++ assets/js/popup.js
@@ -0,0 +1,16 @@
+const popupScreen = document.querySelector(".popup-screen");
+const popupBox = document.querySelector(".popup-box");
+const closeButton = document.querySelector(".close-button");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    popupScreen.classList.add("active");
+    document.getElementById("body").style.overflow = "hidden";
+
+}, 2000);
+});
+closeButton.addEventListener("click", () => {
+  popupScreen.classList.remove("active");
+  document.getElementById("body").style.overflow = "auto";
+
+});
