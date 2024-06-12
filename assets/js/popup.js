77a2bee21f3b6da12a7f78cc05da2ecdf4197c59
Samarth Chauhan <samarthchauhan703@icloud.com>
2024-06-12 13:47:11 +0530

    bbb

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -5,12 +5,36 @@ const closeButton = document.querySelector(".close-button");
 window.addEventListener("load", () => {
   setTimeout(() => {
     popupScreen.classList.add("active");
-    document.getElementById("body").style.overflow = "hidden";
-
-}, 0);
+    document.getElementById("body").style.overflow = "auto";
+  }, 1600);
 });
-closeButton.addEventListener("click", () => {
-  popupScreen.classList.remove("active");
-  document.getElementById("body").style.overflow = "auto";
 
+closeButton.addEventListener("click", () => {
+  hidePopup();
+});
+
+// Function to show the popup
+function showPopup() {
+  popupScreen.classList.add('active');
+
+  // Set a timeout to hide the popup after 5 seconds (5000 milliseconds)
+  //setTimeout(function() {
+  //    hidePopup();
+  //}, 5000); // Adjust the time as needed
+}
+
+// Function to hide the popup
+function hidePopup() {
+  popupScreen.classList.remove('active');
+  popupScreen.classList.add('inactive'); // Add the inactive class to trigger the slide out animation
+}
+
+// Show the popup when needed, for example:
+// showPopup(); // Commented out as it's not needed initially
+
+// Close the popup when clicking anywhere outside of it
+window.addEventListener("click", (event) => {
+  if (event.target === popupScreen) {
+    hidePopup();
+  }
 });
