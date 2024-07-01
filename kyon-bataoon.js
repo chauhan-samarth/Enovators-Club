de020aa39b4eee3903530c0037f1d1c0a626da39
Samarth Chauhan <samarthchauhan703@icloud.com>
2024-07-01 12:18:57 +0530

    jb

--- kyon-bataoon.js
+++ kyon-bataoon.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 9000;
 const SECRET_KEY = process.env.SECRET_KEY;
 
 app.use(bodyParser.json());
